8124cf4ae6f35199c0b069854872e730cf2b3857
Tariel03 <tariel.akmatov@gmail.com>
2023-04-16 16:09:04 +0600

    first commit

---
+++ src/test/java/com/example/sproject/Controllers/MainControllerTest.java
@@ -0,0 +1,39 @@
+package com.example.sproject.Controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MainControllerTest {
+
+    @Test
+    void clientList() {
+    }
+
+    @Test
+    void users() {
+    }
+
+    @Test
+    void showUserInfo() {
+    }
+
+    @Test
+    void uploadPhoto() {
+    }
+
+    @Test
+    void deletePhoto() {
+    }
+
+    @Test
+    void editLastname() {
+    }
+
+    @Test
+    void editFirstname() {
+    }
+
+    @Test
+    void editUsername() {
+    }
+}
